af641a21209802172313473d22911899d8473765
Sean Tran <sgtran@gmail.com>
2021-01-11 23:01:12 -0800

    added Unit 4 pt 2

---
+++ CoinGame.java
@@ -0,0 +1,113 @@
+public class CoinGame
+
+{
+
+    private int startingCoins; // starting number of coins
+
+    private int maxRounds; // maximum number of rounds played
+
+
+
+    public CoinGame(int s, int r)
+
+    {
+
+        startingCoins = s;
+
+        maxRounds = r;
+
+    }
+
+
+
+    /** Returns the number of coins (1, 2, or 3) that player 1 will spend.
+
+     */
+
+    public int getPlayer1Move()
+
+    {
+
+        /* implementation not shown. */
+
+    }
+
+
+
+    /** Returns the number of coins (1, 2, or 3) that player 2 will spend, as described in part (a).
+
+     */
+
+    public int getPlayer2Move(int round)
+
+    {
+        int result;
+        /* to be implemented in part (a) */
+        if ((round % 3) == 0){
+            result = 3;
+        }
+
+        else if ((round % 2) == 0){
+            result = 2;
+        }
+
+        else{
+            result = 1;
+        }
+
+        return  result;
+
+    }
+
+
+
+    /** Plays a simulated game between two players, as described in part (b).
+
+     */
+
+    public void playGame()
+
+    {
+
+        /* to be implemented in part (b) */
+
+        int player1coins = startingCoins;
+        int player2coins = startingCoins;
+        int round = 1;
+
+        while ((round < maxRounds) && (player1coins > 3) && (player2coins > 3) ){
+            getPlayer1Move();
+            getPlayer2Move(round);
+
+            if (getPlayer2Move(round) == getPlayer1Move()){
+                player2coins++;
+            }
+
+            if (((getPlayer2Move(round) - getPlayer1Move()) == 1) || ((getPlayer2Move(round) - getPlayer1Move()) == -1 )){
+                player2coins++;
+            }
+
+            if (((getPlayer2Move(round) - getPlayer1Move()) == 2) || ((getPlayer2Move(round) - getPlayer1Move()) == -2 )){
+                player1coins += 2;
+            }
+
+            round++;
+
+        }
+
+        if (player1coins == player2coins){
+            System.out.println("Tie Game");
+        }
+
+        if (player1coins > player2coins){
+            System.out.println("player 1 wins");
+        }
+
+        if (player1coins < player2coins){
+            System.out.println("player 2 wins");
+        }
+
+
+    }
+
+}
