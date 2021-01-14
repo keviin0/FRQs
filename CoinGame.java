a8097558af061b567e8bdecffd94d4e2821f2f63
Sean Tran <sgtran@gmail.com>
2021-01-14 08:39:22 -0800

    added Unit 4 pt 2

--- CoinGame.java
+++ CoinGame.java
@@ -2,6 +2,13 @@ public class CoinGame
 
 {
 
+    /* (a)   You will write method getPlayer2Move, which returns the number of coins that player 2 will spend in a given round of the game. In the first round of the game, the parameter round has the value 1, in the second round of the game, it has the value 2, and so on. The method returns 1, 2, or 3 based on the following rules.
+
+If round is divisible by 3, then return 3.
+If round is not divisible by 3 but is divisible by 2, then return 2.
+If round is not divisible by 3 and is not divisible by 2, then return 1.
+Complete method getPlayer2Move below by assigning the correct value to result to be returned. */
+
     private int startingCoins = 10; // starting number of coins
 
     private int maxRounds = 5; // maximum number of rounds played
@@ -28,6 +35,7 @@ public class CoinGame
     public int getPlayer1Move(int round)
 
     {
+        // random implementation created by team
 
         /* implementation not shown. */
         if (round == 1){
@@ -92,7 +100,16 @@ public class CoinGame
     public void playGame()
 
     {
+        /*
+The game ends when maxRounds rounds have been played or when a player’s coin count is less than 3 at the end of a round.
 
+At the end of the game, the winner is determined according to the following rules.
+
+If both players have the same number of coins at the end of the game, the method prints "tie game".
+If player 1 has more coins than player 2, the method prints "player 1 wins".
+If player 2 has more coins than player 1, the method prints "player 2 wins".
+(b)   Assume that getPlayer2Move works as specified, regardless of what you wrote in part (a) . You must use getPlayer1Move and getPlayer2Move appropriately to receive full credit.
+*/
         /* to be implemented in part (b) */
 
         int player1coins = startingCoins;
--- Main.java
+++ Main.java
@@ -2,9 +2,11 @@ import java.lang.*;
 public class Main {
 
     public static void main(String[] args){
+        // Unit 4 Q1
         longestStreak execute = new longestStreak();
         execute.longestStreak("CCAAAAATTT!");
 
+        // Unit 4 Q2
         CoinGame executeTwo = new CoinGame(10,5);
 
     }
