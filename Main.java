3df170d1251c74b90bdbad4637a29ad956ed6f99
Sean Tran <sgtran@gmail.com>
2021-02-03 08:28:08 -0800

    test

--- Main.java
+++ Main.java
@@ -4,7 +4,7 @@ public class Main {
     public static void main(String[] args){
        /* // Unit 4 Q1
         longestStreak execute = new longestStreak();
-        execute.longestStreak("CCAAAAATTT!");
+        execute.longestStreak("CCAAAAATTT!"); //
 
         // Unit 4 Q2
         CoinGame executeTwo = new CoinGame(10,5); */
